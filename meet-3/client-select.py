e0c682c0df393087c9e47b3d73a6294c939e3d6a
husinassegaff <husinassegaff15@gmail.com>
2022-02-22 09:04:17 +0700

    feat: add code challenge1 meet3

---
+++ meet-3/client-select.py
@@ -0,0 +1,20 @@
+import socket
+import sys
+
+server_address = ('127.0.0.1', 5000)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(server_address)
+
+sys.stdout.write('>> ')
+
+try:
+    while True:
+        message = str(input())
+        client_socket.send(message.encode())
+        sys.stdout.write(client_socket.recv(1024).decode())
+        sys.stdout.write('>> ')
+
+
+except KeyboardInterrupt:
+    client_socket.close()
+    sys.exit(0)
